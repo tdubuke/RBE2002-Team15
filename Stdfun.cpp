4b5fecd7b2c35b423d1157246a5cb617f1a53327
Harrison Saperstein <harrison.saperstein@gmail.com>
2017-11-28 19:29:51 -0500

    added stuff to std

--- Stdfun.cpp
+++ Stdfun.cpp
@@ -1,2 +1,30 @@
 // Stdfun.cpp
 // File to be used to hold functions not part of a class
+#include "Stdfun.h"
+#include <Encoder.h>
+
+void resetEncoderVal(Encoder eREncoder, Encoder eLEncoder){
+  eREncoder.write(0);
+  eLEncoder.write(0);
+}
+/*
+void addToGlobalXPos(Encoder eREncoder, Encoder eREncoder, float ffXPosition){
+  //change one to positive
+  int iRXTick = eREncoder.read()
+  int iLXTick = eLEncoder.read()
+
+  float ffAvgTicks = (iRXTick + iLXTick) / 2;
+
+  ffXPosition = ffXPosition + ffAvgTicks;
+
+
+}
+*/
+
+float returnDistance(Encoder eEncoder){
+  long ticks = eEncoder.read();
+
+  float distance = ticks*2.75*PI;
+
+  return distance;
+}
--- Stdfun.h
+++ Stdfun.h
@@ -1,2 +1,9 @@
 // Stdfun.h
 // Used for Outside Functions that are not part of a class
+
+#include <Encoder.h>
+#include <Arduino.h>
+
+void resetEncoders(Encoder eREncoder, Encoder eLEncoder);
+//void addToGlobalXPos(Encoder eREncoder, Encoder eREncoder, float ffXPosition);
+long returnEncoderValue(Encoder eEncoder);
